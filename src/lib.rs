8c0e6806ab1fbe79844b6eee54db3e61a889cde4
ferris <ferris@devdroplets.com>
2024-11-17 00:26:04 +0100

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,34 @@
-use std::{cell::OnceCell, str::FromStr, sync::{Arc, OnceLock}};
+use std::{
+    cell::OnceCell,
+    str::FromStr,
+    sync::{Arc, OnceLock},
+};
 
+use eyre::Result;
 use hashbrown::HashMap;
 use pyo3::{
-    ffi::PyTypeObject, intern, prelude::*, types::{PyDict, PyInt, PyType, PyBytes}, PyTypeInfo
+    ffi::PyTypeObject,
+    intern,
+    prelude::*,
+    types::{PyBytes, PyDict, PyInt, PyType},
+    PyTypeInfo,
 };
 use sqlx::{
     any::{AnyConnectOptions, AnyPoolOptions},
     AnyConnection, AnyPool, Row,
 };
 
-
 struct PyTypeLut<T: Clone> {
-    type_lut: dashmap::DashMap<*mut PyTypeObject, T>
+    type_lut: dashmap::DashMap<*mut PyTypeObject, T>,
 }
 
 impl<T: Clone> PyTypeLut<T> {
     fn new() -> Self {
-        PyTypeLut { type_lut: dashmap::DashMap::new() }
+        PyTypeLut {
+            type_lut: dashmap::DashMap::new(),
+        }
     }
-    
+
     fn add_type_explicit(&self, ptype: Bound<'_, PyType>, associated: T) {
         self.type_lut.insert(ptype.as_type_ptr(), associated);
     }
@@ -32,10 +42,11 @@ impl<T: Clone> PyTypeLut<T> {
         // For performance reasons we copy the info from the found type directly to the new type
         //  this means changing the associated type of a root-class will not overwrite derived classes (as they were effectively cached)
         for kv in self.type_lut.iter() {
-            let stype = unsafe { PyType::from_borrowed_type_ptr(ptype.py(), kv.key().clone()) } ;
+            let stype = unsafe { PyType::from_borrowed_type_ptr(ptype.py(), kv.key().clone()) };
             if ptype.is_subclass(&stype).unwrap() {
-                self.type_lut.insert(ptype.as_type_ptr(), kv.value().clone());
-                return Ok(kv.value().clone())
+                self.type_lut
+                    .insert(ptype.as_type_ptr(), kv.value().clone());
+                return Ok(kv.value().clone());
             }
         }
 
@@ -58,21 +69,38 @@ enum TypeAffinity {
 
 #[derive(Clone)]
 struct SqlType {
-    affinity: TypeAffinity
+    affinity: TypeAffinity,
+    nullable: bool,
 }
 
 static PY_TYPE_LUT: OnceLock<PyTypeLut<SqlType>> = OnceLock::new();
 
 struct TypeDef {
     sql_type: &'static str, // Blob, varchar, etc
-    primary_key: bool,
 }
 
+fn try_get_root_sql_type<'py>(anno: &Bound<'py, PyAny>) -> Result<SqlType> {
+    let types_mod = anno.py().import(intern!(anno.py(), "types"))?;
+    let typing_mod = anno.py().import(intern!(anno.py(), "typing"))?;
+
+    let union_type = types_mod.getattr(intern!(anno.py(), "UnionType"))?;
+    let union_typing = typing_mod.getattr(intern!(anno.py(), "_UnionGenericAlias"))?;
+
+    if let Ok(v) = anno.getattr(intern!(anno.py(), "__class__")) {
+        if v.is_exact_instance(&union_type) || v.is_exact_instance(&union_typing) {
+            let tuple_types = v.getattr(intern!(anno.py(), "__args__"))?;
+            // TODO: if <type> | None, where <type> is primitive, use Nullable
+        }
+    }
+
+    Err(eyre::eyre!("No valid sqltype found for {:?}", anno))
+}
 
 // TODO: impl try_from<PyAny> (type annotation) for TypeDef
 
 struct RegisteredModel {
-    schema: HashMap<String, TypeDef>
+    primary_key: String,
+    schema: HashMap<String, TypeDef>,
 }
 
 #[pyclass]
@@ -91,7 +119,7 @@ impl SqlxDb {
         // TODO(perf): share the type_lut to be global lut (reason for dashmap)
         SqlxDb {
             conn: AnyPool::connect_lazy_with(pool_options),
-            registered_models: HashMap::new()
+            registered_models: HashMap::new(),
         }
     }
 
@@ -102,8 +130,8 @@ impl SqlxDb {
         let annotations: &Bound<'py, PyDict> = annotations.downcast()?;
 
         for (k, v) in annotations.iter() {
-            todo!("Get PyType of various desired types/typings (e.g. UnionType, GenericAlias, primitives) by PyType::as_type_ptr into a HashMap to know how to handle them")
-            
+            todo!("Get PyType of various desired types/typings (e.g. UnionType, GenericAlias, primitives) by PyType::as_type_ptr into a HashMap to know how to handle them");
+
             println!("k={:?} v={:?}", k, v);
             let type_name = v
                 .downcast::<PyType>()
@@ -140,12 +168,32 @@ fn pysqlx(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
 
     let lut = PY_TYPE_LUT.get_or_init(|| PyTypeLut::new());
 
-    lut.add_type_explicit(PyInt::type_object(py), SqlType { affinity: TypeAffinity::Integer });
-    lut.add_type_explicit(PyInt::type_object(py), SqlType { affinity: TypeAffinity::Integer });
+    lut.add_type_explicit(
+        PyInt::type_object(py),
+        SqlType {
+            affinity: TypeAffinity::Integer,
+        },
+    );
+    lut.add_type_explicit(
+        PyInt::type_object(py),
+        SqlType {
+            affinity: TypeAffinity::Integer,
+        },
+    );
     // Dicts will be encoded with msgspack or similar (todo: specify encoding method?)
-    lut.add_type_explicit(PyDict::type_object(py), SqlType { affinity: TypeAffinity::Blob });
-    lut.add_type_explicit(PyBytes::type_object(py), SqlType { affinity: TypeAffinity::Blob });
-    
+    lut.add_type_explicit(
+        PyDict::type_object(py),
+        SqlType {
+            affinity: TypeAffinity::Blob,
+        },
+    );
+    lut.add_type_explicit(
+        PyBytes::type_object(py),
+        SqlType {
+            affinity: TypeAffinity::Blob,
+        },
+    );
+
     m.add_class::<SqlxDb>()?;
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
